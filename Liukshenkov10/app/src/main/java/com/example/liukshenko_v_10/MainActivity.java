5593e43eb45128f4318bb71a1a8716645d10850c
LeonidKhromenko <l.khromenko@gmail.com>
2020-12-26 21:16:53 +0700

    creeater

---
+++ Liukshenkov10/app/src/main/java/com/example/liukshenko_v_10/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.liukshenko_v_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void LenyaInduk ()
+    {
+        setContentView(R.layout.activity_main2);
+    }
+}
